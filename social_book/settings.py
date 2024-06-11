26d8617fde215b3d8f4dc745ecc5c86d37b8da17
anishkumarroy <anishroy708@gmail.com>
2024-06-11 18:42:17 +0530

    changes

--- social_book/settings.py
+++ social_book/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'social_book.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'ZLJgpAvGAkCOxrwojEDIfRpJIvJllpgX',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '27576',
-
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ZLJgpAvGAkCOxrwojEDIfRpJIvJllpgX',
+#         'HOST': 'roundhouse.proxy.rlwy.net',
+#         'PORT': '27576',
+
+
+#     }
+# }
 
 
 # Password validation
